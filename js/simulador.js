793893ab1d4f89a6ade26c13ffe03462b11c0107
Diego Siena <diegosienafrancisco@hotmail.com>
2015-04-10 15:47:00 -0300

    Opções de simulação e desenho do vetor

--- js/simulador.js
+++ js/simulador.js
@@ -1,7 +1,53 @@
 $(document).ready(function () {
-    $("#select-tamanho").keydown(function (e) {
+    $("#txt-tamanho").keydown(function (e) {
         if (e.keyCode != 8 && e.keyCode != 9) {
             return false;
         }
     })
+});
+
+function Simular() {
+
+
+
+    //var p1 = $("#vet7").position().left;
+    //var p2 = $("#vet1").position().left;
+    //var range = p1 - p2;
+
+    //$("#vet1").animate({
+    //    top: "+=40"
+    //}, 1000);
+    //$("#vet7").animate({
+    //    top: "-=40"
+    //}, 1000);
+
+    //$("#vet1").animate({
+    //    left: "+=" + range.toString()
+    //}, 1000);
+    //$("#vet7").animate({
+    //    left: "-=" + range.toString()
+    //}, 1000);
+
+
+    //$("#vet1").animate({
+    //    top: "-=40"
+    //}, 1000);
+    //$("#vet7").animate({
+    //    top: "+=40"
+    //}, 1000);
+}
+
+$("#txt-tamanho").change(function () {
+    //var tamanho = $(".vetor li").length;
+    //var txt = parseInt($("#txt-tamanho").val());
+
+    //if (tamanho > txt) {
+    //    for (var i = tamanho; i > txt; i--) {
+    //        $(".vetor li")[i - 1].remove();
+    //    }
+    //} else if (tamanho < txt) {
+    //    for (var i = tamanho; i < txt; i++) {
+    //        $(".vetor").append('<li id="vet' + i.toString() + '" class="col-xs-1">' + (i + 1).toString() + '</li>');
+    //    }
+    //}
 });
