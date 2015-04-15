279d8e6ec791e9e01d5db3baa169178f0ba81940
Diego Siena <diegosienafrancisco@hotmail.com>
2015-04-15 00:44:56 -0300

    Validações e Insertion Sort funcionando

--- js/simulador.js
+++ js/simulador.js
@@ -8,46 +8,114 @@ $(document).ready(function () {
 
 function Simular() {
 
+    if ($('#chk-random').is( ":checked" ) == false) {
+        alert("Método para escolha dos números ainda em desenvolvimento! Selecione o método 'random' !");
+        return;
+    };
 
+    if ($('#chk-animacao').is( ":checked" )) {
+        alert("Animação ainda em desenvolvimento!");
+        return;
+    };
 
-    //var p1 = $("#vet7").position().left;
-    //var p2 = $("#vet1").position().left;
-    //var range = p1 - p2;
+    if ( ($('#cmb-metodo').val() == 1) || ($('#cmb-metodo').val() == 2) || ($('#cmb-metodo').val() == 3) ) {
+        alert("Este método ainda está em desenvolvimento!");
+        return;
+    };
 
-    //$("#vet1").animate({
+    var tamanho = parseInt($("#txt-tamanho").val());
+
+    var vetor = Array();
+
+    for (var i = 0; i < tamanho; i++) {
+        vetor[i] = Math.floor(Math.random() * 100);
+        $('.vetor li:eq(' + i.toString() + ')').html( vetor[i] );
+    };
+
+    if ( $('#cmb-metodo').val() == 0 ) {
+        insertionSort(vetor);
+    };
+
+    // var p1 = $("#vet7").position().left;
+    // var p2 = $("#vet1").position().left;
+    // var range = p1 - p2;
+
+    // $("#vet1").animate({
     //    top: "+=40"
-    //}, 1000);
-    //$("#vet7").animate({
+    // }, 1000);
+    // $("#vet7").animate({
     //    top: "-=40"
-    //}, 1000);
+    // }, 1000);
 
-    //$("#vet1").animate({
+    // $("#vet1").animate({
     //    left: "+=" + range.toString()
-    //}, 1000);
-    //$("#vet7").animate({
+    // }, 1000);
+    // $("#vet7").animate({
     //    left: "-=" + range.toString()
-    //}, 1000);
+    // }, 1000);
 
 
-    //$("#vet1").animate({
+    // $("#vet1").animate({
     //    top: "-=40"
-    //}, 1000);
-    //$("#vet7").animate({
+    // }, 1000);
+    // $("#vet7").animate({
     //    top: "+=40"
-    //}, 1000);
+    // }, 1000);
+}
+
+function insertionSort(items) {
+
+    var len     = items.length,     // number of items in the array
+        value,                      // the value currently being compared
+        i,                          // index into unsorted section
+        j;                          // index into sorted section
+    
+    for (i=0; i < len; i++) {
+    
+        // store the current value because it may shift later
+        value = items[i];
+        
+        /*
+         * Whenever the value in the sorted section is greater than the value
+         * in the unsorted section, shift all items in the sorted section over
+         * by one. This creates space in which to insert the value.
+         */
+        for (j=i-1; j > -1 && items[j] > value; j--) {
+            items[j+1] = items[j];
+        }
+
+        items[j+1] = value;
+
+        printVetor(items);
+    }
+    
+    return items;
+}
+
+function printVetor(vetor) {
+    var html = "";
+    html += '<ul class="col-xs-12 vetor secundario">';
+    for(var i = 0; i < vetor.length; i++) {
+       html += '<li id="vet' + i.toString() + '" class="col-xs-1">' + vetor[i] + '</li>';    
+    }
+    html += '</ul>';
+    $("#panel-vetor").append(html);    
 }
 
 $("#txt-tamanho").change(function () {
-    //var tamanho = $(".vetor li").length;
-    //var txt = parseInt($("#txt-tamanho").val());
 
-    //if (tamanho > txt) {
-    //    for (var i = tamanho; i > txt; i--) {
-    //        $(".vetor li")[i - 1].remove();
-    //    }
-    //} else if (tamanho < txt) {
-    //    for (var i = tamanho; i < txt; i++) {
-    //        $(".vetor").append('<li id="vet' + i.toString() + '" class="col-xs-1">' + (i + 1).toString() + '</li>');
-    //    }
-    //}
+    $("#panel-vetor").html( $('#panel-vetor ul:first') );
+
+    var tamanho = $(".vetor li").length;
+    var txt = parseInt($("#txt-tamanho").val());
+
+    if (tamanho > txt) {
+       for (var i = tamanho; i > txt; i--) {
+           $(".vetor li")[i - 1].remove();
+       }
+    } else if (tamanho < txt) {
+       for (var i = tamanho; i < txt; i++) {
+           $(".vetor").append('<li id="vet' + i.toString() + '" class="col-xs-1">-</li>');
+       }
+    }
 });
